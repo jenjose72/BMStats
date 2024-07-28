cebb6b4a81ebbe4bd315ffa2fdb88d0348b6d38b
jenjose72 <jenjose72@gmail.com>
2024-07-28 17:19:12 +0530

    this is final plsss

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ app.use(passport.session());
 
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,"/frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname,"/my-react-app/dist/index.html"));
 })
 
 app.get('/Stats',async(req,res)=>{
